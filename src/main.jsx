69b8620a87c147c7b347ed9509669bfb4a7fb77f
your-father734 <isaacojulowo734@gmail.com>
2026-09-03 22:59:10 +0100

    modified:   src/main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-import { AuthProvider } from "./store/auth/authStore";
+import { AuthProvider } from "./store/auth/authstore";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
